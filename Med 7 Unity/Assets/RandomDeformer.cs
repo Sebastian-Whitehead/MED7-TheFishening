cf97fcd1be43ae698456a5c4a3e26b5b770d334e
Hatzi2 <72734884+Hatzi2@users.noreply.github.com>
2023-09-27 10:37:52 +0200

    added ground deformer

---
+++ Med 7 Unity/Assets/RandomDeformer.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class RandomDeformer : MonoBehaviour
+{
+    public float deformAmount = 0.5f;
+    public float randomSeed = 0f;
+
+    private MeshFilter meshFilter;
+    private Vector3[] originalVertices;
+    private Vector3[] deformedVertices;
+
+    private void Awake()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        originalVertices = meshFilter.mesh.vertices;
+        deformedVertices = new Vector3[originalVertices.Length];
+    }
+
+    private void Start()
+    {
+        Random.InitState((int)randomSeed);
+        DeformPlane();
+    }
+
+    private void DeformPlane()
+    {
+        for (int i = 0; i < originalVertices.Length; i++)
+        {
+            Vector3 originalVertex = originalVertices[i];
+            Vector3 deformedVertex = originalVertex + Random.insideUnitSphere * deformAmount;
+            deformedVertices[i] = deformedVertex;
+        }
+
+        meshFilter.mesh.vertices = deformedVertices;
+        meshFilter.mesh.RecalculateNormals();
+    }
+}
