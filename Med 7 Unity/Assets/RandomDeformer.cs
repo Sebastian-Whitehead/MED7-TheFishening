2aae67e660d6458dfd82ace2fa83c5d4817c515d
Hatzi2 <72734884+Hatzi2@users.noreply.github.com>
2023-10-10 13:35:40 +0200

    Added comments for the code

--- Med 7 Unity/Assets/RandomDeformer.cs
+++ Med 7 Unity/Assets/RandomDeformer.cs
@@ -2,46 +2,46 @@ using UnityEngine;
 
 public class RandomDeformer : MonoBehaviour
 {
-    public float deformAmount = 0.5f;
-    public float randomSeed = 0f;
+    public float deformAmount = 0.5f; // Amount of deformation to apply
+    public float randomSeed = 0f; // Seed for random number generation
 
-    private MeshFilter meshFilter;
-    private Vector3[] originalVertices;
-    private Vector3[] deformedVertices;
+    private MeshFilter meshFilter; // Reference to the MeshFilter component
+    private Vector3[] originalVertices; // Array to store the original vertices of the mesh
+    private Vector3[] deformedVertices; // Array to store the deformed vertices of the mesh
 
     private void Awake()
     {
-        meshFilter = GetComponent<MeshFilter>();
-        originalVertices = meshFilter.mesh.vertices;
-        deformedVertices = new Vector3[originalVertices.Length];
+        meshFilter = GetComponent<MeshFilter>(); // Get the MeshFilter component attached to the same GameObject
+        originalVertices = meshFilter.mesh.vertices; // Store the original vertices of the mesh
+        deformedVertices = new Vector3[originalVertices.Length]; // Create a new array to store the deformed vertices
     }
 
     private void Start()
     {
-        Random.InitState((int)randomSeed);
-        DeformPlane();
-        UpdateMeshCollider();
+        Random.InitState((int)randomSeed); // Initialize random number generator with the seed
+        DeformPlane(); // Call the method to deform the plane
+        UpdateMeshCollider(); // Call the method to update the MeshCollider
     }
 
     private void DeformPlane()
     {
         for (int i = 0; i < originalVertices.Length; i++)
         {
-            Vector3 originalVertex = originalVertices[i];
-            Vector3 deformedVertex = originalVertex + Random.insideUnitSphere * deformAmount;
-            deformedVertices[i] = deformedVertex;
+            Vector3 originalVertex = originalVertices[i]; // Get the original vertex position
+            Vector3 deformedVertex = originalVertex + Random.insideUnitSphere * deformAmount; // Deform the vertex position by adding a random displacement
+            deformedVertices[i] = deformedVertex; // Store the deformed vertex position
         }
 
-        meshFilter.mesh.vertices = deformedVertices;
-        meshFilter.mesh.RecalculateNormals();
+        meshFilter.mesh.vertices = deformedVertices; // Update the mesh vertices with the deformed vertices
+        meshFilter.mesh.RecalculateNormals(); // Recalculate the vertex normals to ensure proper shading
     }
 
     private void UpdateMeshCollider()
     {
-        MeshCollider meshCollider = GetComponent<MeshCollider>();
+        MeshCollider meshCollider = GetComponent<MeshCollider>(); // Get the MeshCollider component attached to the same GameObject
         if (meshCollider != null)
         {
-            meshCollider.sharedMesh = meshFilter.mesh;
+            meshCollider.sharedMesh = meshFilter.mesh; // Update the shared mesh of the MeshCollider with the deformed mesh
         }
     }
 }
--- Med 7 Unity/Assets/Scripts/ObjectSpawner.cs
+++ Med 7 Unity/Assets/Scripts/ObjectSpawner.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 
 public class ObjectSpawner : MonoBehaviour
 {
-    public List<GameObject> objectsToSpawn;
-    public List<int> spawnQuantities;
-    public float maxSpread = 10f;
+    public List<GameObject> objectsToSpawn; // List of game objects to spawn
+    public List<int> spawnQuantities; // List of quantities for each object to spawn
+    public float maxSpread = 10f; // Maximum spread for object spawning
     public Transform parentObject; // Reference to the empty parent object
     public int randomSeed = 0; // Seed for random number generation
 
     private void Start()
     {
-        SpawnObjects();
+        SpawnObjects(); // Call the method to spawn objects
     }
 
     private void SpawnObjects()
@@ -20,24 +20,24 @@ public class ObjectSpawner : MonoBehaviour
 
         for (int i = 0; i < objectsToSpawn.Count; i++)
         {
-            int quantity = Mathf.Clamp(spawnQuantities[i], 0, int.MaxValue);
+            int quantity = Mathf.Clamp(spawnQuantities[i], 0, int.MaxValue); // Get the quantity to spawn for the current object
             for (int j = 0; j < quantity; j++)
             {
-                Vector3 spawnPosition = transform.position + new Vector3(Random.Range(-maxSpread, maxSpread), 2f, Random.Range(-maxSpread, maxSpread));
-                Quaternion spawnRotation = Quaternion.Euler(Random.Range(0f, 360f), Random.Range(0f, 360f), Random.Range(0f, 360f));
-                GameObject spawnedObject = Instantiate(objectsToSpawn[i], spawnPosition, spawnRotation);
-                spawnedObject.transform.parent = parentObject; // Set the parent of the spawned object
-                BoxCollider collider = spawnedObject.AddComponent<BoxCollider>();
-                Rigidbody rigidbody = spawnedObject.AddComponent<Rigidbody>();
-                StartCoroutine(RemoveColliderAndRigidbodyAfterDelay(collider, rigidbody, 5f));
+                Vector3 spawnPosition = transform.position + new Vector3(Random.Range(-maxSpread, maxSpread), 2f, Random.Range(-maxSpread, maxSpread)); // Calculate a random spawn position
+                Quaternion spawnRotation = Quaternion.Euler(Random.Range(0f, 360f), Random.Range(0f, 360f), Random.Range(0f, 360f)); // Calculate a random spawn rotation
+                GameObject spawnedObject = Instantiate(objectsToSpawn[i], spawnPosition, spawnRotation); // Instantiate the object at the spawn position and rotation
+                spawnedObject.transform.parent = parentObject; // Set the parent of the spawned object to the specified parent object
+                BoxCollider collider = spawnedObject.AddComponent<BoxCollider>(); // Add a box collider to the spawned object
+                Rigidbody rigidbody = spawnedObject.AddComponent<Rigidbody>(); // Add a rigidbody to the spawned object
+                StartCoroutine(RemoveColliderAndRigidbodyAfterDelay(collider, rigidbody, 5f)); // Start a coroutine to remove the collider and rigidbody after a delay
             }
         }
     }
 
     private System.Collections.IEnumerator RemoveColliderAndRigidbodyAfterDelay(BoxCollider collider, Rigidbody rigidbody, float delay)
     {
-        yield return new WaitForSeconds(delay);
-        Destroy(collider);
-        Destroy(rigidbody);
+        yield return new WaitForSeconds(delay); // Wait for the specified delay
+        Destroy(collider); // Destroy the collider component
+        Destroy(rigidbody); // Destroy the rigidbody component
     }
 }
